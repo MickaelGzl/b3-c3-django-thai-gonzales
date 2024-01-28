6e59887fb19e58f63cdc04757a1ce7441bfeb9d2
Mickael Gonzales <mickael.gonzales@hotmail.com>
2024-01-28 18:26:13 +0100

    add function to delete item when click yes on modal

--- passwordManager/tp/urls.py
+++ passwordManager/tp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('sites/create/', views.siteCreate, name='formCreate'),
     path('sites/update/<int:site_id>/', views.siteUpdate, name='formEdit'),
+    path('sites/delete/<int:site_id>/', views.siteDelete, name='siteDelete'),
     path('sites/<int:site_id>/', views.siteView, name='details'),
-    path('sites/delete/', views.siteView, name='siteDelete'),
     
 ]
